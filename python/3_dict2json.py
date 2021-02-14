d821ee72d0f621e9a72781921c5fed077a6637ac
tochika11 <tochika11@gmail.com>
2021-02-14 23:10:19 +0900

    create output directory

--- python/3_dict2json.py
+++ python/3_dict2json.py
@@ -1,6 +1,9 @@
 import pickle
 import json
+import os
 
+OUTPUT_DIR='json/'
+os.makedirs(OUTPUT_DIR, exist_ok=True)
 # -------------------------------------
 # ShieldsLag
 # -------------------------------------
@@ -12,7 +15,7 @@ with open("dict_ShieldsLag.pickle", mode='rb') as f:
 sd = json.dumps(data, sort_keys=True, ensure_ascii=False, indent=2)
 # print(sd)
 
-with open("ShieldsLag.json", mode='w') as f:
+with open(OUTPUT_DIR+"ShieldsLag.json", mode='w') as f:
     json.dump(data, f, sort_keys=True, ensure_ascii=False, indent=2)
 
 # -------------------------------------
@@ -26,7 +29,7 @@ with open("dict_OutOfShieldStartup.pickle", mode='rb') as f:
 sd = json.dumps(data, sort_keys=True, ensure_ascii=False, indent=2)
 # print(sd)
 
-with open("OutOfShieldStartup.json", mode='w') as f:
+with open(OUTPUT_DIR+"OutOfShieldStartup.json", mode='w') as f:
     json.dump(data, f, sort_keys=True, ensure_ascii=False, indent=2)
 
 # -------------------------------------
@@ -40,5 +43,5 @@ with open("dict_CharacterList.pickle", mode='rb') as f:
 sd = json.dumps(data, sort_keys=True, ensure_ascii=False, indent=2)
 # print(sd)
 
-with open("CharacterList.json", mode='w') as f:
+with open(OUTPUT_DIR+"CharacterList.json", mode='w') as f:
     json.dump(data, f, sort_keys=True, ensure_ascii=False, indent=2)
